e582d54f7e0593a82bd79ef3f7b54f6253cc9d41
downside154 <75163801+downside154@users.noreply.github.com>
2023-09-08 11:14:34 +0900

    feat: ranking api testing

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,18 +1,62 @@
-// import React, { useEffect } from "react";
-import React from "react";
+import React, { useEffect } from "react";
 import Fade from "react-reveal";
-// import axios from "axios";
+import axios from "axios";
 
 const About = (props) => {
-  // useEffect(() => {
-  //   const fetchIncreaseData = async () => {
-  //     const res = await axios.get(`https://stocker9.com/api/stocks/increase`);
-  //     console.log(res.data);
-  //   };
-  //   fetchIncreaseData();
-  // }, []);
 
-  if (!props.data) return null;
+  const [increasing, setIncreasing] = React.useState([]);
+  const [decreasing , setDecreasing] = React.useState([]);
+
+  useEffect(() => {
+    const fetchIncreaseData = async () => {
+      const res = await axios.get(`https://stocker9.com/api/stocks/increase`);
+      console.log(res.data);
+     setIncreasing(res.data);
+    };
+    const fetchDecreaseData = async () => {
+      const res = await axios.get(`https://stocker9.com/api/stocks/decrease`);
+      console.log(res.data);
+     setDecreasing(res.data);
+    };
+    fetchIncreaseData();
+    fetchDecreaseData();
+  }, []);
+
+  // const data = [
+  //   { company: "삼성전자", increasePercentage: 1.0 },
+  //   { company: "LG에너지솔루션", increasePercentage: 2.0 },
+  //   { company: "SK하이닉스", increasePercentage: 3.0 },
+  //   { company: "삼성바이오로직스", increasePercentage: 4.0 },
+  //   { company: "POSCO홀딩스", increasePercentage: 5.0 },
+  //   { company: "삼성전자우", increasePercentage: 6.0 },
+  //   { company: "삼성SDI", increasePercentage: 7.0 },
+  //   { company: "LG화학", increasePercentage: 8.0 },
+  //   { company: "현대차", increasePercentage: 9.0 },
+  //   { company: "NAVER", increasePercentage: 999.0 },
+  // ];
+
+  // useEffect(() => {
+  //   data.map((item, index) => {
+  //     console.log(item.company);
+  //     console.log(item.increasePercentage);
+  //     console.log(index);
+  //   });
+  // }
+  // )
+  const increaseList = increasing.map((item, index) => {
+    return (
+      <li key={index}>
+        {index + 1}. {item.company} (⬆{item.increasePercentage} %)
+      </li>
+    );
+  });
+  const decreaseList = decreasing.map((item, index) => {
+    return (
+      <li key={index}>
+        {index + 1}. {item.company} (⬇{item.increasePercentage} %)
+      </li>
+    );
+  });
 
   const profilepic = "images/" + props.data.image;
   const bio = props.data.bio;
@@ -42,16 +86,7 @@ const About = (props) => {
                 <div className="stock-rankings-sub">
                   <h3>Top 10 Performers</h3>
                   <div className="row">
-                    <li> 1 </li>
-                    <li> 2 </li>
-                    <li> 3 </li>
-                    <li> 4 </li>
-                    <li> 5 </li>
-                    <li> 6 </li>
-                    <li> 7 </li>
-                    <li> 8 </li>
-                    <li> 9 </li>
-                    <li> 10 </li>
+                    {increaseList}
                   </div>
                 </div>
 
@@ -59,16 +94,7 @@ const About = (props) => {
                   <h3>Worst 10 Performers</h3>
 
                   <div className="row">
-                    <li> whatev1 </li>
-                    <li> whatev2 </li>
-                    <li> whatev3 </li>
-                    <li> whatev4 </li>
-                    <li> whatev5 </li>
-                    <li> whatev6 </li>
-                    <li> whatev7 </li>
-                    <li> whatev8 </li>
-                    <li> whatev9 </li>
-                    <li> whatev10 </li>
+                    {decreaseList}
                   </div>
                 </div>
               </div>
