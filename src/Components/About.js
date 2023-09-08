23a8dc7278715f7f81e6355e9e78827ad69741dc
downside154 <75163801+downside154@users.noreply.github.com>
2023-09-08 11:35:31 +0900

    feat: ranking api sample data

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,63 +1,48 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Fade from "react-reveal";
-import axios from "axios";
 
 const About = (props) => {
+  const increaseData = [
+    { company: "LG에너지솔루션", increasePercentage: 354.07 },
+    { company: "기아", increasePercentage: 26.45 },
+    { company: "SK하이닉스", increasePercentage: 17.2 },
+    { company: "현대차", increasePercentage: 1.58 },
+    { company: "현대모비스", increasePercentage: 1.1 },
+    { company: "KB금융", increasePercentage: 0.97 },
+    { company: "삼성전자우", increasePercentage: 0.36 },
+    { company: "LG전자", increasePercentage: 0.2 },
+    { company: "LG화학", increasePercentage: 0.17 },
+    { company: "신한지주", increasePercentage: 0.14 },
+  ];
+  const decreaseData = [
+    { company: "삼성전자", decreasePercentage: -5.71 },
+    { company: "POSCO홀딩스", decreasePercentage: -3.63 },
+    { company: "SK이노베이션", decreasePercentage: -1.07 },
+    { company: "NAVER", decreasePercentage: -0.46 },
+    { company: "셀트리온", decreasePercentage: -0.4 },
+    { company: "삼성SDI", decreasePercentage: -0.33 },
+    { company: "삼성물산", decreasePercentage: -0.19 },
+    { company: "삼성바이오로직스", decreasePercentage: -0.13 },
+    { company: "포스코인터내셔널", decreasePercentage: 0.0 },
+    { company: "삼성생명", decreasePercentage: 0.0 },
+  ];
 
-  const [increasing, setIncreasing] = React.useState([]);
-  const [decreasing , setDecreasing] = React.useState([]);
-
-  useEffect(() => {
-    const fetchIncreaseData = async () => {
-      const res = await axios.get(`https://stocker9.com/api/stocks/increase`);
-      console.log(res.data);
-     setIncreasing(res.data);
-    };
-    const fetchDecreaseData = async () => {
-      const res = await axios.get(`https://stocker9.com/api/stocks/decrease`);
-      console.log(res.data);
-     setDecreasing(res.data);
-    };
-    fetchIncreaseData();
-    fetchDecreaseData();
-  }, []);
-
-  // const data = [
-  //   { company: "삼성전자", increasePercentage: 1.0 },
-  //   { company: "LG에너지솔루션", increasePercentage: 2.0 },
-  //   { company: "SK하이닉스", increasePercentage: 3.0 },
-  //   { company: "삼성바이오로직스", increasePercentage: 4.0 },
-  //   { company: "POSCO홀딩스", increasePercentage: 5.0 },
-  //   { company: "삼성전자우", increasePercentage: 6.0 },
-  //   { company: "삼성SDI", increasePercentage: 7.0 },
-  //   { company: "LG화학", increasePercentage: 8.0 },
-  //   { company: "현대차", increasePercentage: 9.0 },
-  //   { company: "NAVER", increasePercentage: 999.0 },
-  // ];
-
-  // useEffect(() => {
-  //   data.map((item, index) => {
-  //     console.log(item.company);
-  //     console.log(item.increasePercentage);
-  //     console.log(index);
-  //   });
-  // }
-  // )
-  const increaseList = increasing.map((item, index) => {
+  const increaseList = increaseData.map((item, index) => {
     return (
       <li key={index}>
         {index + 1}. {item.company} (⬆{item.increasePercentage} %)
       </li>
     );
   });
-  const decreaseList = decreasing.map((item, index) => {
+  const decreaseList = decreaseData.map((item, index) => {
     return (
       <li key={index}>
-        {index + 1}. {item.company} (⬇{item.increasePercentage} %)
+        {index + 1}. {item.company} (⬇{item.decreasePercentage} %)
       </li>
     );
   });
 
+  if (!props.data) return null;
   const profilepic = "images/" + props.data.image;
   const bio = props.data.bio;
 
@@ -85,17 +70,13 @@ const About = (props) => {
               <div className="stock-ranking-container">
                 <div className="stock-rankings-sub">
                   <h3>Top 10 Performers</h3>
-                  <div className="row">
-                    {increaseList}
-                  </div>
+                  <div className="row">{increaseList}</div>
                 </div>
 
                 <div className="stock-rankings-sub">
                   <h3>Worst 10 Performers</h3>
 
-                  <div className="row">
-                    {decreaseList}
-                  </div>
+                  <div className="row">{decreaseList}</div>
                 </div>
               </div>
             </div>
